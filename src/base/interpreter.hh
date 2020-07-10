cc707a5ada93af39ce9ba0a9da59bf0efc39f706
pvt <fisksoppa@LAPTOP-B0VKK0A5.localdomain>
2020-07-10 10:33:31 +0200

    Skelett

---
+++ src/base/interpreter.hh
@@ -0,0 +1,30 @@
+#include <map>
+#include <memory>
+#include <vector>
+#include <string>
+
+namespace interpreter {
+  class expression;
+  class statement;
+  class symbol;
+  class value;
+  class type;
+  class scope;
+  class stack;
+  class code;
+  class program;
+  class userspace;
+
+  class machine;
+  
+  class symbol {
+  public:
+    std::string name;
+    std::
+  }
+
+  class userspace {
+
+
+  }
+}
